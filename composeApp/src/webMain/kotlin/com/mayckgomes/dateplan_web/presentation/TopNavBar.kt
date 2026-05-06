27d0db9f5f83939fee7fc181c652f16a1ffeca63
Mayck Gomes <mayck.gomes2901@gmail.com>
2026-05-06 09:09:46 -0300

    chore: remove unused imports

--- composeApp/src/webMain/kotlin/com/mayckgomes/dateplan_web/presentation/TopNavBar.kt
+++ composeApp/src/webMain/kotlin/com/mayckgomes/dateplan_web/presentation/TopNavBar.kt
@@ -1,18 +1,12 @@
 package com.mayckgomes.dateplan_web.presentation
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.mayckgomes.dateplan_web.components.DownloadButton
 import com.mayckgomes.dateplan_web.components.LogoPlaceholder
 import com.mayckgomes.dateplan_web.components.StyledButton
